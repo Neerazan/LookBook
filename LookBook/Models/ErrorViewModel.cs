0a1771ceec9aac453c00622fa1e9a444fde7573a
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2023-06-22 08:40:03 +0545

    Add project files.

---
+++ LookBook/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LookBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
